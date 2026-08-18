2cda39299d3c73ebdb8430298fafe1eeaadb9b89
Shanhun-Codes <shanhun.codes@gmail.com>
2026-08-18 16:32:35 -0500

    update packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TestController } from './test/test.controller';
-import { TestModule } from './test/test.module';
-import { TestService } from './test/test.service';
 import { AgentsModule } from './agents/agents.module';
 import { LeadsModule } from './leads/leads.module';
 import { AuthModule } from './auth/auth.module';
@@ -11,9 +8,10 @@ import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
 import { DatabaseModule } from './database/database.module';
 import { CommonModule } from './common/common.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TestModule, AgentsModule, LeadsModule, AuthModule, UsersModule, EmailModule, DatabaseModule, CommonModule],
+  imports: [AgentsModule, LeadsModule, AuthModule, UsersModule, EmailModule, DatabaseModule, CommonModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
