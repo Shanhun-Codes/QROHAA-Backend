ef002f6ba75310b236e1640ee65089e787b4b0c8
Shanhun-Codes <shanhun.codes@gmail.com>
2026-09-02 13:52:18 -0500

    fix controller

--- src/agent-app/agent-app.module.ts
+++ src/agent-app/agent-app.module.ts
@@ -3,9 +3,10 @@ import { LeadsModule } from 'src/leads/leads.module';
 import { OpenHousesModule } from 'src/open-houses/open-houses.module';
 import { AgentsModule } from 'src/agents/agents.module';
 import { PropertiesModule } from 'src/properties/properties.module';
+import { AgentAppController } from './agent-app.controller';
 
 @Module({
   imports: [LeadsModule, OpenHousesModule, AgentsModule, PropertiesModule],
-  controllers: [],
+  controllers: [AgentAppController],
 })
 export class AgentAppModule {}
